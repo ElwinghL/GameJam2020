589824515d86d9d9c741205fefade3ef87e8659b
ElwinghL <clement_1996@hotmail.com>
2020-02-01 12:26:45 +0100

    Upgrade misc scripts

--- js/misc.js
+++ js/misc.js
@@ -33,3 +33,15 @@ Object.prototype.forEach = function (callback, thisArg = this) {
     const keys = Object.keys(this);
     keys.forEach(callback, thisArg);
 };
+
+/* OTHER */
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getTruth() {
+    console.log("Tu pues Marie");
+    console.warn("Tu brainstorm dans le vide");
+    console.error("Bon, commence à bosser là !");
+    return "Et genre bcp";
+}
